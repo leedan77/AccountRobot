80083f590ef9732b8262b2b422c5ec5c933e8165
Shawn <san.an.lee@gmail.com>
2016-07-06 01:57:19 +0800

    fix

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function sendWelcomeMessage() {
   }, function(error, response, body){
     if(error) console.error(error);
     else {
-      console.log(response);
+      console.log(body);
     }
   });
 
